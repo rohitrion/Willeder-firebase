0fac99402bc15e5b6935a367a3172e5aaa475a15
roshni gajbhiye <rohitkamui@gmail.comm>
2023-06-22 08:51:24 +0530

    ufinals

--- src/Component/Cards.jsx
+++ src/Component/Cards.jsx
@@ -1,5 +1,4 @@
 
-
 import React, { useState, useEffect } from 'react';
 import {
   MDBCard,
@@ -18,7 +17,7 @@ function Cards() {
     setIsLoading(true);
     try {
       const response = await fetch(
-        `https://api.instantwebtools.net/v1/passenger?page=${page}&size=10`
+        `https://api.instantwebtools.net/v1/passenger?page=${page}&size=20`
       );
       if (response.ok) {
         const jsonData = await response.json();
@@ -63,7 +62,7 @@ function Cards() {
           <MDBCard>
             <MDBCardImage
               position='top'
-              src={item.airline[0]?.logo}
+              src={item?.airline[0]?.logo}
               alt='Airline Logo'
               onError={handleImageError}
             />
@@ -89,111 +88,4 @@ function Cards() {
 }
 
 export default Cards;
-
-
-// import React, { useState, useEffect } from 'react';
-// import {
-//   MDBCard,
-//   MDBCardImage,
-//   MDBCardBody,
-//   MDBCardTitle,
-//   MDBCardText,
-// } from 'mdb-react-ui-kit';
-
-// function Cards() {
-//   const [data, setData] = useState([]);
-//   const [page, setPage] = useState(1);
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [hasError, setHasError] = useState(false);
-
-//   const fetchApiData = async () => {
-//     if (isLoading) return; // Prevent multiple API calls when already loading
-//     setIsLoading(true);
-//     try {
-//       const response = await fetch(
-//         `https://api.instantwebtools.net/v1/passenger?page=${page}&size=10`
-//       );
-//       if (response.ok) {
-//         const jsonData = await response.json();
-//         setData((prevData) => [...prevData, ...jsonData.data]);
-//       } else {
-//         setHasError(true);
-//         console.error('Error retrieving data from the API.');
-//       }
-//     } catch (error) {
-//       setHasError(true);
-//       console.error('Error occurred during API fetch:', error);
-//     } finally {
-//       setIsLoading(false);
-//     }
-//   };
-
-//   const handleScroll = () => {
-//     const scrollThreshold = 200; // Adjust this value as needed
-//     const scrolledToBottom =
-//       window.innerHeight + window.pageYOffset >=
-//       document.documentElement.scrollHeight - scrollThreshold;
-
-//     if (scrolledToBottom) {
-//       setPage((prevPage) => prevPage + 1);
-//     }
-//   };
-
-//   const debounce = (func, delay) => {
-//     let timer;
-//     return function () {
-//       clearTimeout(timer);
-//       timer = setTimeout(func, delay);
-//     };
-//   };
-
-//   useEffect(() => {
-//     fetchApiData();
-//   }, [page]);
-
-//   useEffect(() => {
-//     const debouncedScroll = debounce(handleScroll, 100); // Adjust debounce delay as needed
-//     window.addEventListener('scroll', debouncedScroll);
-//     return () => window.removeEventListener('scroll', debouncedScroll);
-//   }, []);
-
-//   const handleImageError = (e) => {
-//     e.target.onerror = null;
-//     e.target.src = ''; // Provide the path to the fallback logo image
-//   };
-
-//   return (
-//     <div className='card-container'>
-//       {data.map((item, index) => (
-//         <div className='card' key={`${item._id}-${index}`}>
-//           <MDBCard>
-//             <MDBCardImage
-//               position='top'
-//               src={item.airline[0]?.logo}
-//               alt='Airline Logo'
-//               onError={handleImageError}
-//             />
-//             <MDBCardBody>
-//               <MDBCardTitle>
-//                 <b>{item.name}</b>
-//               </MDBCardTitle>
-//               <MDBCardText>
-//                 <div>
-//                   <b>Trips: {item.trips}</b>
-//                   <p>Airline: {item.airline[0]?.name}</p>
-//                   <p>Country: {item.airline[0]?.country}</p>
-//                   <p>Established: {item.airline[0]?.established}</p>
-//                 </div>
-//               </MDBCardText>
-//             </MDBCardBody>
-//           </MDBCard>
-//         </div>
-//       ))}
-//       {isLoading && <p>Loading...</p>}
-//       {hasError && <p>Error occurred while fetching data.</p>}
-//     </div>
-//   );
-// }
-
-// export default Cards;
 
